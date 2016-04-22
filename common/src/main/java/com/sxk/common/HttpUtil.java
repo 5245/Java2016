04c80ee6607decab305251d99ce41917c50cfbcb
sxk5245 <sxk5245@126.com>
2016-04-22 09:57:17 +0800

    初始化

---
+++ common/src/main/java/com/sxk/common/HttpUtil.java
@@ -0,0 +1,12 @@
+package com.sxk.common;
+
+/**
+ * 
+ * @description http工具类
+ * @author sxk
+ * @email sxk5245@126.com
+ * @date 2016年4月21日
+ */
+public class HttpUtil {
+
+}
---
+++ common/src/main/java/com/sxk/common/ImageUtil.java
@@ -0,0 +1,13 @@
+package com.sxk.common;
+/**
+ * @description 图片工具类
+ * @author sxk
+ * @email sxk5245@126.com
+ * @date 2016年4月21日
+ */
+public class ImageUtil {
+    
+    public static void main(String[] args) {
+        
+    }
+}
