c409d75d778ab8632919aea2ed21bab22beffa7b
5245 <sxk5245@126.com>
2017-03-17 11:00:24 +0800

    mybatis自动生成model

---
+++ mybatis-generator/src/main/java/com/sxk/model/WechatCity.java
@@ -0,0 +1,93 @@
+package com.sxk.model;
+
+public class WechatCity {
+    private Integer id;
+
+    private String nationalCode;
+
+    private Byte cityType;
+
+    private String name;
+
+    private String parentCode;
+
+    private String zipCode;
+
+    private Integer provinceId;
+
+    private Integer cityId;
+
+    private Integer districtId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode == null ? null : nationalCode.trim();
+    }
+
+    public Byte getCityType() {
+        return cityType;
+    }
+
+    public void setCityType(Byte cityType) {
+        this.cityType = cityType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getParentCode() {
+        return parentCode;
+    }
+
+    public void setParentCode(String parentCode) {
+        this.parentCode = parentCode == null ? null : parentCode.trim();
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode == null ? null : zipCode.trim();
+    }
+
+    public Integer getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(Integer provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public Integer getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(Integer districtId) {
+        this.districtId = districtId;
+    }
+}
